08fa50e8473ec69a3c2b94b46f92b4a0aeb7ea0a
Elin Simenstad <elin_sim@hotmail.com>
2026-05-14 10:50:17 +0200

    chore: exclude .sandcastle worktrees from Jest test discovery

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ const createJestConfig = nextJest({ dir: './' });
 const config = {
     coverageProvider: 'v8',
     testEnvironment: 'node',
-    testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
+    testPathIgnorePatterns: ['/node_modules/', '/e2e/', '/.sandcastle/'],
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/$1',
     },
